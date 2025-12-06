749ec4b55050ac5510fcad47a468a0f6e0797f31
ahmedghaly <ahmedghaly0767@gmail.com>
2025-12-06 03:56:59 +0200

    ci: Deploy 🎉

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'core/widgets/flutter_error_details_view.dart';
 import 'engz_app.dart';
 
 void main() async {
+  // testing deploy
   WidgetsFlutterBinding.ensureInitialized();
   final dir = await getApplicationDocumentsDirectory();
 
